c1452b8a1e435ccf07a6e4dcb3b5fc19c0792ec4
J <johnnyqbui7@gmail.com>
2020-09-15 01:08:43 -0500

    moved dialog out to component, cleanup styles

--- components/ChatBox.js
+++ components/ChatBox.js
@@ -1,42 +1,20 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {Text, View, ScrollView, StyleSheet} from 'react-native';
-import {Avatar, Card, ListItem, Button, Icon} from 'react-native-elements';
+import {ActivityIndicator, View, StyleSheet} from 'react-native';
 
 import UserInput from './UserInput';
 import ChatAgent from './ChatAgent';
+import DialogBox from './DialogBox';
 
 const API_ENDPOINT =
   'https://gist.githubusercontent.com/pcperini/97fe41fc42ac1c610548cbfebb0a4b88/raw/cc07f09753ad8fefb308f5adae15bf82c7fffb72/cerebral_challenge.json';
 
-const useAppendDialogs = (agentName, currentQuestion, text) => {
-  const [dialogs, setDialogs] = useState([]);
-
-  setDialogs([
-    ...dialogs,
-    {
-      user: agentName,
-      text: currentQuestion.question,
-    },
-    {
-      user: 'You',
-      text,
-    },
-    {
-      user: agentName,
-      text: `${text} is not a valid answer.`,
-    },
-  ]);
-
-  return [dialogs, setDialogs];
-};
 export default ChatBox = () => {
-  const [agentName, setAgentName] = useState('');
   const [loading, setLoading] = useState(false);
-  const [dialogs, setDialogs] = useState([]);
-  const [endDialog, setEndDialog] = useState(false);
-  const [allQuestions, setAllQuestions] = useState([]);
-  const [currentQuestion, setCurrentQuestion] = useState({});
   const [loadingMessage, setLoadingMessage] = useState(false);
+  const [allQuestions, setAllQuestions] = useState([]);
+  const [agentName, setAgentName] = useState('');
+  const [dialogs, setDialogs] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState({});
   const scrollRef = useRef(null);
 
   useEffect(() => {
@@ -61,119 +39,111 @@ export default ChatBox = () => {
     }, 100);
   }, [dialogs, loadingMessage]);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     loadingMessage &&
-  //       setDialogs([
-  //         ...dialogs,
-  //         {
-  //           user: agentName,
-  //           text: '*Thinking*',
-  //         },
-  //       ]);
-  //   }, 2000);
-  // }, [loadingMessage]);
+  const onPressSubmit = text => {
+    const formatText = text.trim().toLowerCase();
 
-  console.log({currentQuestion});
+    setLoadingMessage(true);
+    setDialogs([
+      ...dialogs,
+      {
+        user: agentName,
+        text: currentQuestion.question,
+      },
+      {
+        user: 'You',
+        text,
+      },
+      {
+        user: agentName,
+        text: '*Thinking*',
+      },
+    ]);
+
+    // fake load/thinking
+    setTimeout(() => {
+      setLoadingMessage(false);
+      let valid = false;
+
+      if (Array.isArray(currentQuestion.validation)) {
+        valid = currentQuestion.validation.includes(formatText);
+      }
+
+      if (typeof currentQuestion.validation === 'string') {
+        const regex = RegExp(currentQuestion.validation);
+        valid = regex.test(formatText);
+      }
+
+      if (typeof currentQuestion.validation === 'boolean') {
+        valid = currentQuestion.validation;
+      }
+
+      if (!valid) {
+        setDialogs([
+          ...dialogs,
+          {
+            user: agentName,
+            text: currentQuestion.question,
+          },
+          {
+            user: 'You',
+            text,
+          },
+          {
+            user: agentName,
+            text: `Sorry, ${text} is not a valid answer.`,
+          },
+        ]);
+      } else {
+        const nextQuestion = allQuestions.find(question =>
+          typeof currentQuestion.paths === 'object'
+            ? currentQuestion.paths[formatText] === question.id
+            : currentQuestion.paths === question.id,
+        );
+        setCurrentQuestion(nextQuestion);
+      }
+    }, 2000);
+  };
 
   return (
-    <Card>
-      <ChatAgent agent={agentName} />
-      <Card.Divider />
+    <View style={styles.chatBox}>
       {loading ? (
-        <Text style={styles.question}>...loading</Text>
+        <ActivityIndicator size="large" />
       ) : (
-        // Chat Dialog
-        <ScrollView ref={scrollRef}>
-          {dialogs.map(({user, text}) => (
-            <Text style={{fontSize: 32}}>
-              {user}: {text}
-            </Text>
-          ))}
-          {!loadingMessage && (
-            <Text style={{fontSize: 32}}>
-              {agentName}: {currentQuestion.question}
-            </Text>
-          )}
-        </ScrollView>
+        <>
+          <ChatAgent agent={agentName} />
+          <DialogBox
+            scrollRef={scrollRef}
+            dialogs={dialogs}
+            loadingMessage={loadingMessage}
+            agentName={agentName}
+            currentQuestion={currentQuestion}
+          />
+          <UserInput
+            disabled={!currentQuestion.paths}
+            onPressSubmit={onPressSubmit}
+          />
+        </>
       )}
-      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-        <UserInput
-          disable={!currentQuestion.paths}
-          onPressSubmit={text => {
-            const formatText = text.trim().toLowerCase();
-            setDialogs([
-              ...dialogs,
-              {
-                user: agentName,
-                text: currentQuestion.question,
-              },
-              {
-                user: 'You',
-                text,
-              },
-              {
-                user: agentName,
-                text: '*Thinking*',
-              },
-            ]);
-            let valid = false;
-
-            if (Array.isArray(currentQuestion.validation)) {
-              valid = currentQuestion.validation.includes(formatText);
-            }
-
-            if (typeof currentQuestion.validation === 'string') {
-              const regex = RegExp(currentQuestion.validation);
-              valid = regex.test(formatText);
-            }
-
-            if (typeof currentQuestion.validation === 'boolean') {
-              valid = currentQuestion.validation;
-            }
-
-            setLoadingMessage(true);
-
-            // fake load/thinking
-            setTimeout(() => {
-              setLoadingMessage(false);
-
-              if (!valid) {
-                setDialogs([
-                  ...dialogs,
-                  {
-                    user: agentName,
-                    text: currentQuestion.question,
-                  },
-                  {
-                    user: 'You',
-                    text,
-                  },
-                  {
-                    user: agentName,
-                    text: `${text} is not a valid answer.`,
-                  },
-                ]);
-              } else {
-                const nextQuestion = allQuestions.find(question =>
-                  typeof currentQuestion.paths === 'object'
-                    ? currentQuestion.paths[formatText] === question.id
-                    : currentQuestion.paths === question.id,
-                );
-                setCurrentQuestion(nextQuestion);
-              }
-            }, 2000);
-          }}
-        />
-      </View>
-    </Card>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   chatBox: {
     flex: 1,
-    padding: 20,
-    backgroundColor: 'blue',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+    backgroundColor: 'white',
+    margin: 25,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9,
   },
 });
--- components/UserInput.js
+++ components/UserInput.js
@@ -1,30 +1,66 @@
 import React, {useState} from 'react';
-import {Text} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {StyleSheet} from 'react-native';
 import {Input} from 'react-native-elements';
 
 const UserInput = props => {
-  const {onPressSubmit} = props;
-  const [value, onChangeText] = useState('');
+  const {onPressSubmit, disabled} = props;
+  const [value, setValue] = useState('');
 
   return (
     <Input
+      containerStyle={styles.userInputContainer}
+      inputContainerStyle={styles.inputContainer}
+      rightIconContainerStyle={styles.rightIcon}
+      maxLength={200}
+      disabled={disabled}
+      inputStyle={styles.input}
       multiline
       placeholder="Type Here..."
-      onChangeText={text => onChangeText(text)}
+      onChangeText={text => setValue(text)}
       value={value}
-      rightIcon={
-        <Text
-          onPress={() => {
-            if (props.disable) return;
-            onPressSubmit(value);
-            onChangeText('');
-          }}>
-          Send
-        </Text>
-      }
+      rightIcon={{
+        type: 'font-awesome',
+        name: 'paper-plane-o',
+        color: 'white',
+        onPress: () => {
+          if (disabled) return;
+
+          onPressSubmit(value);
+          setValue('');
+        },
+      }}
     />
   );
 };
 
 export default UserInput;
+
+const styles = StyleSheet.create({
+  userInputContainer: {
+    backgroundColor: '#ebf4f6',
+    padding: 10,
+  },
+  inputContainer: {
+    borderBottomWidth: 0,
+  },
+  input: {
+    flex: 3,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#dfebf8',
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginRight: 10,
+    padding: 10,
+    paddingBottom: 50,
+    borderWidth: 1,
+    borderRadius: 2,
+  },
+  rightIcon: {
+    flex: 1,
+    flexDirection: 'row',
+    height: '100%',
+    backgroundColor: '#5093fd',
+    borderRadius: 3,
+  },
+});
